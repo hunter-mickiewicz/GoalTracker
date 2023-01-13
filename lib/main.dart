6290c0c0c0870c849614c7c2e5442577d66cf38e
Hunter Mickiewicz <hunter.mickiewicz@gmail.com>
2023-01-13 07:59:09 -0700

    working on buttons to pull up date selectors for goal creation page

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyAppState extends ChangeNotifier {
 
   var selectedIndex = 0;
 
-  void addGoal() {
+  void addGoal(DateTime start, DateTime end, double perc) {
     goalList
         .add(gc.GoalClass(DateTime.now(), DateTime.utc(2023, 12, 31), 0.69));
     notifyListeners();
@@ -55,6 +55,9 @@ class _Tracker extends State<Tracker> {
         break;
       case 1:
         page = ManagePage();
+        break;
+      case 2:
+        page = GoalCreatorPage();
         break;
       default:
         page = Placeholder();
@@ -161,11 +164,58 @@ class _ManagePageState extends State<ManagePage> {
   Widget build(BuildContext ctx) {
     var appState = ctx.watch<MyAppState>();
 
+    void _createGoal() {
+      appState.selectedIndex = 2;
+      appState.notifyListeners();
+    }
+
     return Scaffold(
         floatingActionButton: FloatingActionButton(
-      onPressed: appState.addGoal,
-      backgroundColor: Color.fromARGB(0, 0, 0, 0),
+      onPressed: _createGoal,
       child: Icon(Icons.add),
+    ));
+  }
+}
+
+class GoalCreatorPage extends StatefulWidget {
+  @override
+  State<GoalCreatorPage> createState() => _GoalCreatorPageState();
+}
+
+class _GoalCreatorPageState extends State<GoalCreatorPage> {
+  DateTime? begin;
+  String beginString = "Select Start Date";
+  DateTime? end;
+  String endString = "Select End Date";
+
+  void _dateSelection() {
+    DialogRoute<DateTime>(
+        context: context,
+        builder: (BuildContext context) {
+          return DatePickerDialog(
+            initialEntryMode: DatePickerEntryMode.calendarOnly,
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now(),
+            lastDate: DateTime.now(),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          OutlinedButton(
+            onPressed: () {
+              _dateSelection();
+            },
+            child: Text("Start Date"),
+          ),
+          OutlinedButton(onPressed: _dateSelection, child: Text("EndDate"))
+        ]),
+      ],
     ));
   }
 }
