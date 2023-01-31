2cb2e4055e83d1063624b852a3aa4f283bcdb3ad
Hunter Mickiewicz <hunter.mickiewicz@gmail.com>
2023-01-31 08:53:56 -0700

    fleshed out unit tests for GoalClass

--- test/GoalClass_test.dart
+++ test/GoalClass_test.dart
@@ -1,40 +1,54 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:namer_app/GoalClass.dart';
 import 'package:test/test.dart';
 
 main() {
   group('Goal Class', () {
     var goal = GoalClass(DateTime(2022), DateTime(2023), 12.0, "test");
-    test('Beginning date should be initialized', () {
+    test('Information should be initialized', () {
       expect(goal.begin, DateTime(2022));
-    });
-    test('End date should be initialized', () {
       expect(goal.end, DateTime(2023));
-    });
-    test('Percentage should be initialized', () {
       expect(goal.percent, 0.12);
-    });
-    test('Name should be initialized', () {
       expect(goal.name, "test");
     });
 
-    var goal2 = GoalClass(DateTime(2022), DateTime(2023), 12.0, "test");
-    goal2.editGoal(DateTime(2023), DateTime(2024), 40.0, "update");
-    test('Beginning date should be changed', () {
-      expect(goal2.begin, DateTime(2023));
+    test('Goal information should be changed', () {
+      goal.editGoal(DateTime(2023), DateTime(2024), 40.0, "update");
+      expect(goal.begin, DateTime(2023));
+      expect(goal.end, DateTime(2024));
+      expect(goal.percent, 0.40);
+      expect(goal.name, "update");
     });
-    test('End date should be changed', () {
-      expect(goal2.end, DateTime(2024));
-    });
-    test('Percentage should be changed', () {
-      expect(goal2.percent, 0.40);
-    });
-    test('Name should be changed', () {
-      expect(goal2.name, "update");
+
+    test('Usable date should be properly formatted', () {
+      expect(goal.usableDate(DateTime(2022, 1, 1)), "1/1/2022");
     });
 
     test('Milestones should be empty', () {
       expect(goal.milestones.length, 0);
     });
-    //add/edit milestone
+
+    test('Milestones should be properlly added and hashable', () {
+      goal.updateMilestones(DateTime(2022), "milestone test");
+      expect(goal.milestones.length, 1);
+      expect(goal.milestones[goal.usableDate(DateTime(2022))]![0],
+          "milestone test");
+    });
+
+    test('Milestones on the same date are properly added and hashable', () {
+      //goal.updateMilestones(DateTime(2022), "milestone test");
+      goal.updateMilestones(DateTime(2022), "second test");
+      expect(goal.milestones.length, 1);
+      expect(goal.milestones[goal.usableDate(DateTime(2022))]!.length, 2);
+      expect(
+          goal.milestones[goal.usableDate(DateTime(2022))]![1], "second test");
+    });
+    //milestone on same date
+
+    test('GetStringPercent returns the proper percentage as a string', () {
+      expect(goal.getStringPercent(), '40.0');
+    });
   });
 }
